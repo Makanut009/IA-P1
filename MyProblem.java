18e569a8b120242f4243082160bc03bc4b86fe03
ArianH23 <rodrigo.arian.huapaya@est.fib.upc.edu>
2020-03-19 16:17:12 +0100

    Experimento especial finalizado
    Co-authored-by: Enric Lamarca Ferrés <enric.lamarca@est.fib.upc.edu>
    Co-authored-by: Jordi Cluet <45299503+Makanut009@users.noreply.github.com>

--- MyProblem.java
+++ MyProblem.java
@@ -34,22 +34,20 @@ public class MyProblem {
                 System.out.println("Introduce el número mínimo de réplicas por fichero: 5");
                 int nrep = 5; //in.nextInt();
                 
-                System.out.println("Introduce el generador de solucion inicial, 1 o 2: 1");
-                int sol = 1; //0
-                
+                int sol = 2;
                 while(sol!= 1 && sol!=2){
-                    System.out.println("Introduce el generador de solucion inicial, 1 o 2: ");
+                    System.out.println("Introduce el generador de solucion inicial, 1 o 2: 2");
                     sol = in.nextInt();
                 }
                 
-                System.out.println("Introduce tipo de heuristico, 1 o 2 o 3: 2");
+                System.out.println("Introduce tipo de heuristico, 1 o 2 o 3: 1");
                 int heu = 2; //in.nextInt();
                 
-                System.out.print("Introduce tipo de successor, 1, 2 o 3: ");
-                int succ = in.nextInt();
+                System.out.println("Introduce tipo de successor, 1, 2 o 3: 3");
+                int succ = 3; //in.nextInt();
                 
-                System.out.println("Iteraciones que hacer sobre el problema: 10 \n");
-                int ite = 10; //in.nextInt();
+                System.out.println("Iteraciones que hacer sobre el problema: 1 \n");
+                int ite = 1; //in.nextInt();
 
                 for(int i = 0; i < ite;++i){
 
@@ -63,16 +61,19 @@ public class MyProblem {
                     int seed2;//in.nextInt();
                     //if (seed2 < 0) seed2 = random.nextInt();
 
-                    if (i == 0) { seed1 = 1771455837; seed2 = 2003815554;}
-                    else if (i == 1) { seed1 = 891091758; seed2 = -202376362;}
-                    else if (i == 2) { seed1 = -804509587; seed2 = -658628491;}
-                    else if (i == 3) { seed1 = 1238774172; seed2 = -711265905;}
-                    else if (i == 4) { seed1 = 7009249; seed2 = 809791697;}
-                    else if (i == 5) { seed1 = -573801870; seed2 = 571569998;}
-                    else if (i == 6) { seed1 = 1969224113; seed2 = 1840351992;}
-                    else if (i == 7) { seed1 = -1869634747; seed2 = -542952602;}
-                    else if (i == 8) { seed1 = 1131403503; seed2 = -2007456259;}
-                    else { seed1 = -489086994; seed2 = 354937147;}
+                    seed1 = 1234;
+                    seed2 = 1234;
+
+                    // if (i == 0) { seed1 = 1771455837; seed2 = 2003815554;}
+                    // else if (i == 1) { seed1 = 891091758; seed2 = -202376362;}
+                    // else if (i == 2) { seed1 = -804509587; seed2 = -658628491;}
+                    // else if (i == 3) { seed1 = 1238774172; seed2 = -711265905;}
+                    // else if (i == 4) { seed1 = 7009249; seed2 = 809791697;}
+                    // else if (i == 5) { seed1 = -573801870; seed2 = 571569998;}
+                    // else if (i == 6) { seed1 = 1969224113; seed2 = 1840351992;}
+                    // else if (i == 7) { seed1 = -1869634747; seed2 = -542952602;}
+                    // else if (i == 8) { seed1 = 1131403503; seed2 = -2007456259;}
+                    // else { seed1 = -489086994; seed2 = 354937147;}
 
                     System.out.println("Semillas: " + seed1 + " " + seed2);
 
@@ -97,7 +98,7 @@ public class MyProblem {
                     //     MySimulatedAnnealingSearch(estado, heu, succ, lambda);
 
                     long elapsedTime = System.currentTimeMillis() - tini;
-                    System.out.println("Tiempo de ejecución: " + elapsedTime +"ms\n");
+                    System.out.println("Tiempo de ejecución: " + elapsedTime +" ms\n");
                 }
             }
         }
